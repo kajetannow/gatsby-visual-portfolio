f1cf94e133538192bda336bbe172f144c654da5b
unknown <kajetannow>
2020-07-03 18:21:51 +0200

    Updates

---
+++ component---src-pages-404-js-b2924f339ad70ddc9ae1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return c}));var u=t("q1tI"),a=t.n(u),r=t("7oih");function c(){return a.a.createElement(r.a,null,a.a.createElement("h1",null,"404"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b2924f339ad70ddc9ae1.js.map
